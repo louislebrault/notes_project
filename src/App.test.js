c4b00f030bf2155c81d15c758020ee4a19e25be8
Louis Lebrault <louis.lebrault@gmail.com>
2018-09-14 17:09:34 +0200

    refactored tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactTestUtils from 'react-dom/test-utils';
+import {
+  scryRenderedDOMComponentsWithClass,
+  findRenderedDOMComponentWithClass,
+  Simulate } from 'react-dom/test-utils';
+
 import App from './App';
 
+let container;
+let app;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  app = ReactDOM.render(<App />, container);
+});
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.unmountComponentAtNode(container);
 });
 
 it('display given notes', () => {
+  ReactDOM.unmountComponentAtNode(container);
+  
   const notes = ['note 1', 'note 2'];
+  app = ReactDOM.render(<App notes={notes}/>, container);
 
-  const div = document.createElement('div');
-  const app = ReactDOM.render(<App notes={notes}/>, div);
-
-  const result = ReactTestUtils.scryRenderedDOMComponentsWithClass(app, 'note');
+  const result = scryRenderedDOMComponentsWithClass(app, 'note');
   expect(result.length).toBe(notes.length)
 });
+
+it('display an input field to add notes', () => {
+  const result = findRenderedDOMComponentWithClass(app, 'newNote');
+  expect(result).toBeTruthy();
+});
+
+it('add a note when clicking on button if input is not empty', () => {
+  const input = findRenderedDOMComponentWithClass(app, 'newNote');
+  const button = findRenderedDOMComponentWithClass(app, 'addButton');
+
+  simulateUserInput('change', input, 'a');
+  simulateUserInput('click', button);
+
+  findRenderedDOMComponentWithClass(app, 'note');
+});
+
+function simulateUserInput(event, target, value) {
+  if (event === 'click') {
+    Simulate.click(target);
+  } else if (event === 'change') {
+    Simulate.change(target, { target: { value }});
+  } else {
+    throw new Error ('simulateUserInput :: unknown event :' + event);
+  }
+}
